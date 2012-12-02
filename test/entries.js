66e824596a6680fb8664919d4bd946baaa7a071f
Willie Miller <WillieMiller3@gmail.com>
2012-12-02 18:03:56 -0500

    Changed Drog object layout and the way the library is initalized.
    This is a cleaner way to handle the blog. By creating an object to hold
    all the entries and other information in the instance, the concept
    of a Drog becomes reentrant.

--- test/entries.js
+++ test/entries.js
@@ -1,37 +1,40 @@
 var assert = require("assert"),
     path = require('path'),
     should = require('should'),
-    drog = require("../drog.js");
+    wrench = require('wrench'),
+    drog = require("../lib/drog.js");
 
-var simpleTest = path.join(__dirname, 'simpleTest');
+var simpleTest = path.join(__dirname, 'simpleTest'),
+    testFiles = path.join(__dirname, 'testFiles');
 
 describe("Drog", function(){
     var testDrog;
     beforeEach(function(done){
-        drog.init({
+        testDrog = new drog.Drog({
             root : simpleTest,
-            success : function(drogEntries){
-                testDrog = drogEntries;        
+            success : function(){
                 done();
             },
             failure : done
-            
         });
     });
     describe("#entries()", function(){
         it("should return a list of entries", function(done){
-            testDrog.should.be.an.instanceof(Array);
-            testDrog.length.should.equal(1);
+            var entries = testDrog.entries();
+            entries.should.be.an.instanceof(Array);
+            entries.length.should.equal(1);
             done();
         }); 
         it("should read the json definition properly", function(){
-            testDrog[0].should.be.an.instanceof(drog.DrogEntry);
-            testDrog[0].title.should.eql("Test Entry 1");
-            testDrog[0].root.should.eql(path.join(simpleTest,"testEntry1"));
-            testDrog[0].date.should.equal(new Date("December 1, 2012"));
+            var entries = testDrog.entries();
+            entries[0].should.be.an.instanceof(drog.DrogEntry);
+            entries[0].title.should.eql("Test Entry 1");
+            entries[0].root.should.eql(path.join(simpleTest,"testEntry1"));
+            entries[0].date.should.equal(new Date("December 1, 2012"));
         });
         it("should read the file loading variables properly", function(){
-            testDrog[0].content.should.eql(
+            var entries = testDrog.entries();
+            entries[0].content.should.eql(
             "This is a test blog, it doesn's actually say anything\n" 
             );
         });
